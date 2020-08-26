b7fb9be29fbf5e238bea47d213c56c0188a7462b
yash mantri <mantriyash2025@gmail.com>
2020-08-26 20:30:23 +0530

    Doubly Linked List

---
+++ Remove_Duplicates.py
@@ -0,0 +1,51 @@
+def remove_duplicates(self):
+    cur = self.head
+    d = {}
+    while cur:
+      if cur.data not in d:
+        d[cur.data] = 1
+        cur = cur.next
+      else:
+        nxt = cur.next
+        self.delete_node(cur)
+        cur = nxt      
+  
+def delete_node(self, node):
+    cur = self.head
+    while cur:
+      if cur == node and cur == self.head:
+        # Case 1:
+        if not cur.next:
+          cur = None 
+          self.head = None
+          return
+
+        # Case 2:
+        else:
+          nxt = cur.next
+          cur.next = None 
+          nxt.prev = None
+          cur = None
+          self.head = nxt
+          return 
+
+      elif cur == node:
+        # Case 3:
+        if cur.next:
+          nxt = cur.next 
+          prev = cur.prev
+          prev.next = nxt
+          nxt.prev = prev
+          cur.next = None 
+          cur.prev = None
+          cur = None
+          return
+
+        # Case 4:
+        else:
+          prev = cur.prev 
+          prev.next = None 
+          cur.prev = None 
+          cur = None 
+          return 
+      cur = cur.next
